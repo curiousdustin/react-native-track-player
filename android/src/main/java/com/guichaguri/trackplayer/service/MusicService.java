aaa8af6cd30689ce3c2c0da1caaadc3522dcb050
Dustin Bahr <dustin@curiousmedia.com>
2019-12-13 16:20:59 -0600

    Attempt to fix error related to notification channel.
    "Fatal Exception: android.app.RemoteServiceException: Bad notification for startForeground: java.lang.RuntimeException: invalid channel for service notification: null"
    
    Followed approach suggested here:
    https://github.com/react-native-kit/react-native-track-player/pull/723

--- android/src/main/java/com/guichaguri/trackplayer/service/MusicService.java
+++ android/src/main/java/com/guichaguri/trackplayer/service/MusicService.java
@@ -2,7 +2,7 @@ package com.guichaguri.trackplayer.service;
 
 import android.app.Activity;
 import android.app.Notification;
-import android.app.NotificationChannel;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -11,13 +11,13 @@ import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.LocalBroadcastManager;
 import android.support.v4.media.session.MediaButtonReceiver;
-
 import android.support.v4.media.session.MediaSessionCompat;
 import com.facebook.react.HeadlessJsTaskService;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
 import com.facebook.react.jstasks.HeadlessJsTaskConfig;
+import com.guichaguri.trackplayer.service.Utils;
 import javax.annotation.Nullable;
 
 /**
@@ -74,12 +74,8 @@ public class MusicService extends HeadlessJsTaskService {
 
             // Checks whether there is a React activity
             if(reactContext == null || !reactContext.hasCurrentActivity()) {
-                String channel = null;
-
-                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    channel = NotificationChannel.DEFAULT_CHANNEL_ID;
-                }
-
+                String channel = Utils.getNotificationChannel((Context) this);
+                
                 // Sets the service to foreground with an empty notification
                 startForeground(1, new NotificationCompat.Builder(this, channel).build());
                 // Stops the service right after
